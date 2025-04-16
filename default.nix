c21748ae4e04b5ae6a15dd796cafd9b3e0efda7b
Fabio Elvedi <fabio.elvedi@elvfab.net>
2025-04-16 14:03:39 +0200

    update installPhase to generate .desktop

--- default.nix
+++ default.nix
@@ -19,7 +19,7 @@ in pkgs.stdenv.mkDerivation {
     pkgs.nlohmann_json
     pkgs.gtest.dev
   ];
-  
+
   configurePhase = ''
     cmake -GNinja $src -DGTest_DIR=${pkgs.gtest}/lib/cmake/GTest
   '';
@@ -28,13 +28,31 @@ in pkgs.stdenv.mkDerivation {
     cmake --build .
   '';
 
-
   installPhase = ''
+    # Install binary
     mkdir -p $out/bin
     cp hyprview $out/bin/
 
+    # Install QML resources
     mkdir -p $out/share/hyprview/qml
     cp -r src/ui/qml/* $out/share/hyprview/qml
+
+    # Install .desktop file for Wofi/app launchers
+    mkdir -p $out/share/applications
+    cat > $out/share/applications/hyprview.desktop <<EOF
+[Desktop Entry]
+Name=HyprView
+Comment=Monitor layout tool for Hyprland
+Exec=hyprview
+Icon=display
+Terminal=false
+Type=Application
+Categories=Utility;
+EOF
+
+    # Optional: install icon
+    # mkdir -p $out/share/icons/hicolor/256x256/apps
+    # cp res/icon.png $out/share/icons/hicolor/256x256/apps/hyprview.png
   '';
 
   env = {
